801762fe29b4a7e5921eff47932037d826c09381
sigma748345678 <ellychandr5654@gmail.com>
2026-02-24 09:33:13 +0100

    Implement basic chatbot functionality in mybot.py
    my bot python

---
+++ mybot.py
@@ -0,0 +1,20 @@
+person = {"hello": "Hi!", #slovari vsegda v peremenoy
+"bye": "Goodbye!",
+"name": "My name is EllyBot",
+"age": "I am 0 years old",
+"python": "Python is awesome! and lua too...",
+"help": "Commands: hello, bye, name, age, python, and to exit you need `",
+"pizdec ti tupoy": "idi nahuy",
+"suka": "...",
+"poka": "pokakayesh doma" #slovar dla bota sam bot nize
+}
+while True:  #beskonechni cikl
+    a = input("you:").lower() #lover dla togo chtobi koda bila tak: HELLO to ono otvechalo
+    if a in person: #chem gushe les if else if else
+        print("bot:", person[a]) #vivod
+    elif a == '`': # eto na exit
+        print("bye")
+        break #break eto tipo konec tipo liv
+    else:
+        print("???") #bot ne znaet
+# ya iz budushego ispravi eto govno pls
