85e07ffe4994dd42daa1b9ae4c0f4fd9bf698af3
sigma748345678 <ellychandr5654@gmail.com>
2026-02-25 18:17:53 +0100

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -1,16 +1,55 @@
 import time
 import os
-person = {"hello": "Hi!", #slovari vsegda v peremenoy
-"bye": "Goodbye!",
-"name": "My name is EllyBot",
-"age": "I am 0 years old",
-"python": "Python is awesome! and lua too...",
-"help": "Commands: hello, bye, name, age, python, and to exit you need ` , and - this is shutdown pc, timer = timer",
-"pizdec ti tupoy": "idi nahuy",
-"tupoy": "sam takoy",
-"EYY I'M NOT STUPID": "who said",
-"suka": "...",
-"poka": "pokakayesh doma" #slovar dla bota sam bot nize
+person = {
+    "hello": "Hi there!",
+    "hi": "Hello!",
+    "hey": "Hey!",
+    "bye": "Goodbye!",
+    "goodbye": "See you later!",
+    "name": "My name is EllyBot",
+    "age": "I am 0 years old",
+    "python": "Python is awesome!",
+    "lua": "Lua is cool too!",
+    "help": "Commands: hello, hi, bye, name, age, python, lua, joke, timer, shutdown",
+    "joke": "Why did the programmer quit his job? Because he didn't get arrays!",
+    "funny": "I love to laugh!",
+    "sad": "Don't be sad, cheer up!",
+    "happy": "That's great!",
+    "thanks": "You're welcome!",
+    "thank you": "No problem!",
+    "morning": "Good morning!",
+    "night": "Good night!",
+    "food": "I love pizza!",
+    "drink": "Tea is nice",
+    "music": "I like chiptune music!",
+    "song": "I don't have ears, but I can play tunes!",
+    "game": "Do you like games?",
+    "play": "I can simulate a dice for you!",
+    "dice": "Rolling a dice... 🎲",
+    "yes": "Alright!",
+    "no": "Okay!",
+    "maybe": "Hmm, maybe...",
+    "weather": "I don't know the weather, but I hope it's sunny!",
+    "date": "Every day is a good day!",
+    "love": "Love is in the air!",
+    "hate": "I prefer coding to hate.",
+    "angry": "Take a deep breath.",
+    "fun": "Fun is coding!",
+    "bored": "I can tell you a joke!",
+    "confused": "Don't worry, I understand!",
+    "stupid": "Everyone learns at their own pace.",
+    "smart": "Thanks!",
+    "yes please": "As you wish!",
+    "no thanks": "No problem!",
+    "ok": "Ok!",
+    "oops": "It's fine, mistakes happen!",
+    "cool": "I'm cool, right?",
+    "amazing": "Thanks, I try!",
+    "wow": "Wow indeed!",
+    "help me": "Just type a command!",
+    "shutdown": "Use '-' to shutdown PC",
+    "random": "I can do random stuff!",
+    "secret": "I know a secret..."
 }
 while True:  #beskonechni cikl
     a = input("you:").lower().strip() #lover dla togo chtobi koda bila tak: HELLO to ono otvechalo a string che bi kogda bili probeli toze otvechal
@@ -21,6 +60,15 @@ while True:  #beskonechni cikl
         if g == "yes":
             os.system("shutdown /s") #virubanie pc
             time.sleep(5)
+            break
+    elif a == "time":
+        concurrent_time = time.strftime("%H:%M:%S")
+        print(concurrent_time)
+        lol = input("for what you use this, you dont have clock on pc?").lower().strip()
+        if lol == "yes":
+            print("okeeeeeyyyy")
+        else:
+            print(".....")
             break
     elif a == "timer":
         kot = int(input("secunds:"))
