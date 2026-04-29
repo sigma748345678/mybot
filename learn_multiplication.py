661f4ca08d0e3c86e3fa6b636fa5f1471189bf4a
sigma748345678 <ellychandr5654@gmail.com>
2026-04-29 17:10:11 +0200

    i think this bot......

---
+++ learn_multiplication.py
@@ -0,0 +1,44 @@
+import random
+import time
+from colorama import Fore, Style
+from random import randint
+math_advice = """
+1. Learn the multiplication table – Knowing times tables by heart makes calculations much faster.
+2. Use the distributive property – Break numbers into easier parts (e.g., 12 × 4 = (10 × 4) + (2 × 4)).
+3. Multiply in parts – Split large numbers into smaller ones to simplify the problem.
+4. Use patterns – Notice patterns like multiplying by 10, 100, or 5 to make quick mental math easier.
+5. Practice regularly – The more you practice, the faster and more accurate you become
+"""
+def mult():
+    score = 0
+    trys = 0
+    while trys < 5:
+        print(Style.RESET_ALL)
+        random_multiplication1 = randint(1, 10)
+        random_multiplication2 = randint(1, 10)
+        true_answer = random_multiplication1 * random_multiplication2
+        time.sleep(1)
+        print("what is", random_multiplication1, "*", random_multiplication2)
+        try:
+            user_input = int(input("answer: "))
+            if user_input == true_answer:
+                print(Fore.GREEN + "True!!!")
+                trys += 1
+                score += 1
+            else:
+                print(Fore.RED + "False!")
+                trys += 1
+        except:
+            print(Fore.RED + "you can only enter numbers there")
+        if trys == 5:
+            print(Fore.BLUE + "your score:", score, "/", "5")
+            percent = round((score / 5) * 100, 2)
+            print(Fore.GREEN + str(percent) + "%")
+print(Fore.CYAN + "math test!")
+while True:
+    print(Style.RESET_ALL)
+    user_choice = input(Fore.LIGHTCYAN_EX + "to start, type 'start' (you can write 'tips' and it will give you the top 5 tips to learn multiplication): ")
+    if user_choice == "start":
+        mult()
+    elif user_choice == "tips":
+        print(Fore.RED + math_advice)
