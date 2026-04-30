e907b529bf2c876b8c66063eace6989dc8a5207e
sigma748345678 <ellychandr5654@gmail.com>
2026-04-30 17:05:23 +0200

    Refactor user input options for math tests

--- learn_multiplication.py
+++ learn_multiplication.py
@@ -42,11 +42,123 @@ def mult():
             percent_cheat = round((score / 5) * 100, 2)
             print(Fore.GREEN + str(percent_cheat) + "%")
             break # this break just return in the main menu
+
+def addi():
+    score = 0
+    trys = 0
+    cheating = 0
+    while trys < 5:
+        print(Style.RESET_ALL)
+        random_addion1 = randint(1, 10)
+        random_addion2 = randint(1, 10)
+        true_answer = random_addion1 + random_addion2
+        time.sleep(1)
+        print("what is", random_addion1, "+", random_addion2)
+        try:
+            user_input = int(input("answer: "))
+            if user_input == true_answer:
+                print(Fore.GREEN + "True!!!")
+                trys += 1
+                score += 1
+            else:
+                print(Fore.RED + "False!")
+                trys += 1
+                print(Fore.LIGHTBLUE_EX + "True answer:", true_answer)
+        except ValueError:
+            print(Fore.LIGHTRED_EX + "you can only enter numbers there.")
+            cheating += 1
+        if trys == 5:
+            print(Fore.BLUE + "your score:", score, "/", trys)
+            percent = round((score / 5) * 100, 2)
+            print(Fore.GREEN + str(percent) + "%")
+        elif cheating == 2:
+            time.sleep(2)
+            print(Fore.BLUE + "your score:", score, "/", trys)
+            percent_cheat = round((score / 5) * 100, 2)
+            print(Fore.GREEN + str(percent_cheat) + "%")
+            break # this break just return in the main menu
+
+def sub():
+    score = 0
+    trys = 0
+    cheating = 0
+    while trys < 5:
+        print(Style.RESET_ALL)
+        random_sub1 = randint(1, 10)
+        random_sub2 = randint(1, 10)
+        true_answer = random_sub1 - random_sub2
+        time.sleep(1)
+        print("what is", random_sub1, "-", random_sub2)
+        try:
+            user_input = int(input("answer: "))
+            if user_input == true_answer:
+                print(Fore.GREEN + "True!!!")
+                trys += 1
+                score += 1
+            else:
+                print(Fore.RED + "False!")
+                trys += 1
+                print(Fore.LIGHTBLUE_EX + "True answer:", true_answer)
+        except ValueError:
+            print(Fore.LIGHTRED_EX + "you can only enter numbers there.")
+            cheating += 1
+        if trys == 5:
+            print(Fore.BLUE + "your score:", score, "/", trys)
+            percent = round((score / 5) * 100, 2)
+            print(Fore.GREEN + str(percent) + "%")
+        elif cheating == 2:
+            time.sleep(2)
+            print(Fore.BLUE + "your score:", score, "/", trys)
+            percent_cheat = round((score / 5) * 100, 2)
+            print(Fore.GREEN + str(percent_cheat) + "%")
+            break # this break just return in the main menu
+
+def div():
+    score = 0
+    trys = 0
+    cheating = 0
+    while trys < 5:
+        print(Style.RESET_ALL)
+        random_div1 = randint(1, 10)
+        random_div2 = randint(1, 10)
+        true_answer = random_div1 / random_div2
+        time.sleep(1)
+        print("what is", random_div2, "/", random_div1)
+        try:
+            user_input = int(input("answer: "))
+            if user_input == true_answer:
+                print(Fore.GREEN + "True!!!")
+                trys += 1
+                score += 1
+            else:
+                print(Fore.RED + "False!")
+                trys += 1
+                print(Fore.LIGHTBLUE_EX + "True answer:", true_answer)
+        except ValueError:
+            print(Fore.LIGHTRED_EX + "you can only enter numbers there.")
+            cheating += 1
+        if trys == 5:
+            print(Fore.BLUE + "your score:", score, "/", trys)
+            percent = round((score / 5) * 100, 2)
+            print(Fore.GREEN + str(percent) + "%")
+        elif cheating == 2:
+            time.sleep(2)
+            print(Fore.BLUE + "your score:", score, "/", trys)
+            percent_cheat = round((score / 5) * 100, 2)
+            print(Fore.GREEN + str(percent_cheat) + "%")
+            break # this break just return in the main menu
+
 print(Fore.CYAN + "math test!")
 while True:
     print(Style.RESET_ALL)
-    user_choice = input(Fore.LIGHTCYAN_EX + "to start, type 'start' (you can write 'tips' and it will give you the top 5 tips to learn multiplication): ").strip().lower()
-    if user_choice == "start":
+    user_choice = input(Fore.LIGHTCYAN_EX + "to start, type 'multiplication/addion/subtraction/division' (you can write 'tips' and it will give you the top 5 tips to learn multiplication): ").strip().lower()
+    if user_choice == "multiplication":
         mult()
+    elif user_choice == "addion":
+        addi()
+    elif user_choice == "subtraction":
+        sub()
+    elif user_choice == "division":
+        div()
     elif user_choice == "tips":
         print(Fore.RED + math_advice)
